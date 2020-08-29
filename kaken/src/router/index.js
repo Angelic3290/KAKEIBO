bcd0bcbb6de6714c5c3fa8726b2a86591a295588
Hiroki Okugaki <hiroki@Macbook-Air.local>
2020-08-29 17:39:26 +0900

    月次集計結果画面作成

--- kaken/src/router/index.js
+++ kaken/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import MonthlyReport from '@/components/MonthlyReport'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/hoge',
+      name: 'MonthlyReport',
+      component: MonthlyReport
     }
   ]
 })
