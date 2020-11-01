7010654e8b9dfa0d99c37a30bbbe8f0d98c311bb
Hiroki Okugaki <hiroki@Macbook-Air.local>
2020-11-01 15:39:00 +0900

    localstorageを追加

--- kaken/src/App.vue
+++ kaken/src/App.vue
@@ -1,13 +1,45 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <input v-model="name" @change="setName" />
+    {{ getName }}
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  localStorage: {
+    name: {
+      type: String
+    },
+  },
+  data() {
+    return {
+      name: '' 
+    }
+  },
+  computed: {
+    getName() {
+      return this.$localStorage.get('name')
+    },
+  },
+  mounted() {
+    if (localStorage.name) {
+      this.name = localStorage.name;
+    }
+  },
+  watch: {
+    name() {
+      console.log('hoge')
+    },
+  },
+  methods: {
+    setName() {
+      this.$localStorage.set('name', this.name)
+    },
+  },
 }
 </script>
 
